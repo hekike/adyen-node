721a561162677feb1d60f3120465b1fe33dfa2db
Sebastian Gronewold <sebastian.gronewold@feelgood.de>
2015-04-15 16:39:11 +0200

    Some minor test refactorings
    As the adyen account that was used for testing does not exist anymore, we cannot use a real payment window for testing. In fact the window should be mocked out anyways. For now it should be fine to verify that the URL for adyen is properly build up.

--- test/hpp.js
+++ test/hpp.js
@@ -15,7 +15,6 @@ describe('HPP', function () {
       HMACKey: '123456',
       skinCode: 'Iix4eLo8',
       merchantAccount: 'JessePiscaerCOM',
-      orderData: 'XY 1 year subscription',
       paymentAmount: 1000,
       currencyCode: 'EUR'
     });
@@ -37,23 +36,18 @@ describe('HPP', function () {
 
     it('should generate url and merchantReference', function (done) {
       hppPayment.generateRequest(function (err, url, merchantReference) {
+        console.log(url);
         expect(url).to.be.a('string');
         expect(merchantReference).to.be.a('string');
         expect(merchantReference).to.have.string('PAYMENT-');
 
         expect(url).to.match(/((http|https):\/\/(\w+:{0,1}\w*@)?(\S+)|)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/);
-        done();
-      });
-    });
 
-    it('should be a valid payment window', function (done) {
-      hppPayment.generateRequest(function (err, url, merchantReference) {
-        request(url, function (err, res, body) {
-          expect(body).to.have.string(merchantReference);
-          expect(body).to.have.string('JessePiscaerCOM');
-          expect(body).to.have.string('XY 1 year subscription');
-          done();
-        });
+        expect(url).to.contain('JessePiscaerCOM');
+        expect(url).to.contain('EUR');
+        expect(url).to.contain('Iix4eLo8');
+
+        done();
       });
     });
   });
